075193f9bf60f4aae4dd6ffb54b79af359d6cfae
VS Code User <user@example.com>
2026-06-07 13:48:24 +0530

    Update and push src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,26 @@ function doSomething()
 
  let count=1;
  
- let count1=2;
  //count='a';
  console.log(count);
-  console.log(count1);
+
+ //Variables
+
+
+ let a: number=20;
+ let b: string="TestQA";
+ let c: boolean=true;
+ let d: any=30;
+ let e: number[]=[1,2,3,4,5];
+ let f: string[]=["Test","QA","Automation"];
+ let g: any[]=[1,"Test",true,2,"QA",false];
+
+ console.log("Number is",+a);
+ console.log("String is"+b);
+ console.log("Boolean is"+c);
+ console.log("Any type is"+d);
+ console.log("Number Array is"+e);
+ console.log("String Array is"+f);
+ console.log("Any Array is"+g); 
+
+
