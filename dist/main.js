10cc9fe5cfa78dc4a78615b0eae7cba3b906cf94
VS Code User <user@example.com>
2026-06-07 13:26:01 +0530

    Add dist output files

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+"use strict";
+function log(message) {
+    console.log(message);
+}
+log("Hello, World!");
+function doSomething() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    //cannot access 'i' outside of the loop, as it is block-scoped due to the use of 'let'.
+    //console.log(i);
+}
+doSomething();
+let count = 1;
+let count1 = 2;
+//count='a';
+console.log(count);
+console.log(count1);
+//# sourceMappingURL=main.js.map
