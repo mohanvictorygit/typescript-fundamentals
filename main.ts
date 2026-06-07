14d7457fc457ae001e87bc3a85ee8e65344eae84
VS Code User <user@example.com>
2026-06-07 12:21:57 +0530

    Add main.ts

---
+++ main.ts
@@ -0,0 +1,21 @@
+function log(message: string)
+{
+
+    console.log(message);
+}
+
+log("Hello, World!");
+
+function doSomething()
+{
+    for (let i = 0; i < 5; i++)
+    {
+        console.log(i);
+
+    }
+
+
+    //cannot access 'i' outside of the loop, as it is block-scoped due to the use of 'let'.
+    //console.log(i);
+}
+ doSomething();
